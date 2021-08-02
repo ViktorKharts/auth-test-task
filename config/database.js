c3c31f451bd71e217bc50acc399b1cf5ec562097
ViktorKharts <victor.kharchenko1@gmail.com>
2021-08-02 07:53:50 +0300

    switch db config to prod

--- config/database.js
+++ config/database.js
@@ -1,23 +1,23 @@
 // prod schema
-// module.exports = {
-//     'connection': {
-//         'host': 'eu-cdbr-west-01.cleardb.com',
-//         'user': 'b127db1a1d5054',
-//         'password': 'aec2863c',
-//         'database': 'heroku_0d2e2729559a45e'
-//     },
-//     'users_table': 'users',
-//     'countries_table': 'countries'
-// };
-
-// dev schema
 module.exports = {
     'connection': {
-        'host': '127.0.0.1',
-        'user': 'root',
-        'password': '123456789',
-        'database': 'user_schema'
+        'host': 'eu-cdbr-west-01.cleardb.com',
+        'user': 'b127db1a1d5054',
+        'password': 'aec2863c',
+        'database': 'heroku_0d2e2729559a45e'
     },
     'users_table': 'users',
     'countries_table': 'countries'
 };
+
+// dev schema
+// module.exports = {
+//     'connection': {
+//         'host': '127.0.0.1',
+//         'user': 'root',
+//         'password': '123456789',
+//         'database': 'user_schema'
+//     },
+//     'users_table': 'users',
+//     'countries_table': 'countries'
+// };
